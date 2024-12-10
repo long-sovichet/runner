8c19bede0de1bf7b5e55b6d064885900b59762ed
long-sovichet <harryjamespottter516@gmail.com>
2024-12-10 23:55:42 +0700

    Nothing to commit

--- src/main/java/dev/harry/runner/RunnerApplication.java
+++ src/main/java/dev/harry/runner/RunnerApplication.java
@@ -3,11 +3,16 @@ package dev.harry.runner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import foo.WelcomeMessage;
+
 @SpringBootApplication
 public class RunnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerApplication.class, args);
+
+		var welcomeMessage = new WelcomeMessage();
+		System.out.println(welcomeMessage.getWelcomeMessage());
 	}
 
 }
